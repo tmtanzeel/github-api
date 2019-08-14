e17a5ceaaa6ca23b126ddc31f6ea5e8daecee447
tmtanzeel <tmtanzeel9@gmail.com>
2019-08-14 21:19:43 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,40 +1,10 @@
 import React, { Component } from "react";
+import GitHub from './components/GitHub';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      items: [],
-      isLoaded: false
-    };
-  }
-
-  componentDidMount() {
-    fetch(
-      "https://api.github.com/users/tmtanzeel"
-    )
-      .then(res => res.json())
-      .then(json => {
-        this.setState({
-          isLoaded: true,
-          total_repos: json.public_repos
-        });
-      });
-  }
-
   render() {
-    var { isLoaded, total_repos } = this.state;
-    if (!isLoaded) return <div>Loading...</div>;
     return (
-      <div>
-        <ul>
-          {
-            <li >
-              Public repos : {total_repos}
-            </li>
-          }
-        </ul>
-      </div>
+      <GitHub />
     );
   }
 }
---
+++ src/components/GitHub.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import GitHubCalendar from 'react-github-calendar';
+
+const GitHub = () => {
+  return (
+    <GitHubCalendar username="tmtanzeel" blockSize={10} blockMargin={4} />
+  );
+}
+
+export default GitHub;
